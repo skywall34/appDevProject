a650f7086f1b86cfae2ab52299799e4f1317cfb0
[Fan_h] <[Fan.Hong@utexas.edu]>
2018-08-18 17:46:47 -0500

    Android Login version2

---
+++ Myblog 2/app/src/main/java/com/example/hongfan/myblog/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.hongfan.myblog
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+
+
+
+
+const val RC_SIGN_IN = 123
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signin_button.visibility = View.VISIBLE
+        name.visibility = View.VISIBLE
+        signin_button.setSize(SignInButton.SIZE_STANDARD);
+        signin_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+            val acct = GoogleSignIn.getLastSignedInAccount(this)
+            if (acct != null) {
+                signin_button.visibility = View.GONE
+                name.text=acct.displayName
+                name.visibility=View.VISIBLE
+
+            }
+        }
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            signin_button.visibility = View.GONE
+            name.text=account.displayName
+            name.visibility=View.VISIBLE
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+           signin_button.visibility = View.VISIBLE
+            name.text="fail"
+            name.visibility=View.VISIBLE
+        }
+
+    }
+
+    fun openpost(view: View){
+        val intent = Intent(this, post::class.java)
+        startActivity(intent)
+    }
+
+}
---
+++ Myblog 2/app/src/main/java/com/example/hongfan/myblog/PostModel.kt
@@ -0,0 +1,9 @@
+package com.example.hongfan.myblog
+
+class createPost(
+        val title: String,
+        val country: String,
+        val statte: String,
+        val city:String,
+        val theme:String,
+        val descripting:String)
